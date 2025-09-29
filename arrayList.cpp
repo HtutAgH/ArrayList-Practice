dcf673074a15e0fdad9d5be90527a96821013d90
HtutAgH <106645767+HtutAgH@users.noreply.github.com>
2025-09-29 14:40:50 -0700

    Complete the manipulators codes in cpp files

--- arrayList.cpp
+++ arrayList.cpp
@@ -31,3 +31,43 @@ bool arrayList::isItemAtEqual(int index, const complexNumber& item) const {
     const complexNumber& x = data[index];
     return x.getReal() == item.getReal() && x.getIm() == item.getIm();
 }
+
+void arrayList::insertAt(int index, const complexNumber& item) {
+    if (index < 0 || index > length) throw std::out_of_range("index out of range");
+    if (isFull()) throw std::overflow_error("list is full");
+    // shift right [index..length-1]
+    for (int i = length; i > index; --i) {
+        data[i] = data[i - 1];
+    }
+    data[index] = item;
+    ++length;
+}
+
+void arrayList::insertEnd(const complexNumber& item) {
+    if (isFull()) throw std::overflow_error("list is full");
+    data[length++] = item;
+}
+
+void arrayList::removeAt(int index) {
+    if (index < 0 || index >= length) throw std::out_of_range("index out of range");
+    // shift left [index+1..length-1]
+    for (int i = index; i < length - 1; ++i) {
+        data[i] = data[i + 1];
+    }
+    --length;
+}
+
+complexNumber arrayList::retrieveAt(int index) const {
+    if (index < 0 || index >= length) throw std::out_of_range("index out of range");
+    return data[index]; 
+}
+
+void arrayList::replaceAt(int index, const complexNumber& item) {
+    if (index < 0 || index >= length) throw std::out_of_range("index out of range");
+    data[index] = item;
+}
+
+void arrayList::clearList() {
+    // no need to delete[]; we keep capacity and reuse the buffer
+    length = 0;
+}
